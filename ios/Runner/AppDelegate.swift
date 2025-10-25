81f5012e1af22edc2863cc7c54085b0f0ca18865
murtadhaahmed <mahmed@elitesoft.iq>
2025-10-26 00:34:34 +0300

    feat: integrate OneSignal SDK in AppDelegate and update project configuration for notification handling

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import OneSignalFramework
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
+    OneSignal.initialize("b136e33d-56f0-4fc4-ad08-8c8a534ca447", withLaunchOptions: launchOptions)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
